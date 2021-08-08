4c3d8d26d8d131854e0c63ab87966f68cba95b6f
Carolinacapote <carolina.capote28@gmail.com>
2021-08-08 23:46:55 +0000

    [DOCUMENTATION](add docummentation)

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" """
+"""Unittest module to test the amenity class"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """Class to test the amenity class that inherits from test_basemodel"""
 
     def __init__(self, *args, **kwargs):
-        """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Method to test the amenity class"""
         new = self.value()
         self.assertEqual(type(new.name), str)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""Unittest module to test the Base_model class"""
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -9,38 +9,38 @@ import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """Class to test the base_model class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """Method to test the base_model class"""
         pass
 
     def tearDown(self):
+        """Method to test the base_model class"""
         try:
             os.remove('file.json')
         except:
             pass
 
     def test_default(self):
-        """ """
+        """Method to test the base_model class"""
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """Method to test the base_model class"""
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """Method to test the base_model class"""
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -48,7 +48,7 @@ class test_basemodel(unittest.TestCase):
             new = BaseModel(**copy)
 
     def test_save(self):
-        """ Testing save """
+        """ Testing save method from base_model class"""
         i = self.value()
         i.save()
         key = self.name + "." + i.id
@@ -57,41 +57,41 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """Testing the str method of base_model class"""
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
+        """Testing the to_dict method of base_model class"""
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
+        """Testing the class base_model without kwargs"""
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ """
+        """Testing the error messages of the base_model class"""
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ """
+        """ Testing the id attribute of the base_model class"""
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """Testing the created at attribute of the base_model class"""
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """Testing the updated_at attribute of the base_model class"""
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
