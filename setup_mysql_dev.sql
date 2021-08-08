56f716032d6b117b6498354e77410399a3eb5d5c
Adriana Echeverri <adriana.er98@hotmail.com>
2021-08-08 12:45:03 -0500

    [SQL] (Prepare SQL databases)

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,1 +1,5 @@
--- Prepares SQL server for connection
+-- Prepare SQL server for connection, creates table
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON 'hbnb_dev_db' TO 'hbnb_dev'@'localhost';
+GRANT SELECT PRIVILEGES ON 'performance_schema' TO 'hbnb_dev'@'localhost';
