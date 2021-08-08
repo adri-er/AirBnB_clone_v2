ef4b87d28c3864b2136c9f3c5969ba0c1e689530
Adriana Echeverri <adriana.er98@hotmail.com>
2021-08-08 18:20:18 +0000

    [SQL] (fix permissions)

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,10 @@
 -- Prepare SQL server for connection, creates table
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT PRIVILEGES ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
+--GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
+--GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+--GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
