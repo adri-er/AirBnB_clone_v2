4b29aec2d82c3385e05f9cbffecdcf4628b06b01
Carolinacapote <carolina.capote28@gmail.com>
2021-08-09 21:38:26 +0000

    [REFACTOR](Database storage)

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,10 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -10,10 +14,12 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = Column(String(60), nullable=False, primary_key=True)
+            self.created_at = Column(DateTime, default=datetime.utcnow(),
+                                     nullable=False)
+            self.updated_at = Column(DateTime, default=datetime.utcnow(),
+                                     nullable=False)
+            # storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +37,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +48,15 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        key = '_sa_instance_state'
+        if hasattr(dictionary, key):
+            dictionary.pop(key)
+
         return dictionary
+
+        def delete(self):
+            """public instance method that delete the current instance from
+            storage by calling the delete() method
+            """
+            storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ Class inherited from BaseModel that describes a State """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='states', cascade='all, delete-orphan')
+    else:
+        from models.engine.file_storage import FileStorage
+        cities = []
+        for key, value in FileStorage.__objects.items():
+            cls_id = key.split('.')
+            cls = cls_id[0]
+
+            if cls == 'City' and value.state_id == self.id:
+                cities.append(value)
