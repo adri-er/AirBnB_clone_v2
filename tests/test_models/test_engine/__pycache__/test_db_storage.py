6c27ab5e587f0ee79771e1ed1411b3c07f70c3dc
Adriana Echeverri <adriana.er98@hotmail.com>
2021-08-11 18:07:58 -0500

    [TEST] (test storage)

--- tests/test_models/test_engine/__pycache__/test_db_storage.py
+++ tests/test_models/test_engine/__pycache__/test_db_storage.py
@@ -22,7 +22,7 @@ run = os.system
 
 class TestHBNBCommand(unittest.TestCase):
     """
-    this class test HBNBCommand class and its behavior
+    this class test sql storage
     """
 
     def setUp(self) -> None:
@@ -55,46 +55,23 @@ class TestHBNBCommand(unittest.TestCase):
         result = pep8style.check_files(['console.py'])
         self.assertEqual(result.total_errors, 0)
 
-    def test_aa_create(self):
-        """ create tests """
-        no_stdout = " > /dev/null 2>&1"
-        run("rm file.json " + no_stdout)
-        run("echo 'create State' | ./console.py" + no_stdout)
-        run("echo 'all' | ./console.py" + no_stdout)
-        with open("file.json", 'r') as f:
-            temp = json.load(f)
-        self.assertTrue(temp)
-
-    def test_ab_create(self):
-        """ create tests """
-        no_stdout = " > /dev/null 2>&1"
-        run("rm file.json " + no_stdout)
-        run('echo create State name="California" | ./console.py' + no_stdout)
-        run("echo 'all' | ./console.py" + no_stdout)
-        with open("file.json", 'r') as f:
-            temp = json.load(f)
-        self.assertTrue(temp)
-
     def test_ac_create(self):
         """ create tests """
         no_stdout = " > /dev/null 2>&1"
-        run("rm file.json " + no_stdout)
-        command = 'echo create City state_id="1" name="San_Francisco" '
-        command = command + '| ./console.py '
+        command = 'echo "create State name="California""'
+        sp_1 = " HBNB_MYSQL_USER=hbnb_test HBNB_MYSQL_PWD=hbnb_test_pwd "
+        sp_2 = "HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_test_db "
+        sp_3 = "HBNB_TYPE_STORAGE=db HBNB_ENV=test "
+        command = command + '|{}{}{}./console.py '.format(sp_1, sp_2, sp_3)
         run(command + no_stdout)
-        run("echo 'all' | ./console.py" + no_stdout)
-        with open("file.json", 'r') as f:
-            temp = json.load(f)
-        self.assertTrue(temp)
 
-    def test_ac_create(self):
-        """ create tests """
-        no_stdout = " > /dev/null 2>&1"
-        run("rm file.json " + no_stdout)
-        command = 'echo create City state_id="1" name="San_Francisco" '
-        command = command + '| ./console.py '
+        command = 'echo "SELECT COUNT(*) FROM states"'
+        sp_1 = " HBNB_MYSQL_USER=hbnb_test HBNB_MYSQL_PWD=hbnb_test_pwd "
+        sp_2 = "HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_test_db "
+        sp_3 = "HBNB_TYPE_STORAGE=db HBNB_ENV=test "
+        command = command + '|{}{}{}./console.py '.format(sp_1, sp_2, sp_3)
+        std_out = StringIO()
+        sys.stdout = std_out
         run(command + no_stdout)
-        run("echo 'all' | ./console.py" + no_stdout)
-        with open("file.json", 'r') as f:
-            temp = json.load(f)
-        self.assertTrue(temp)
+        output = std_out.getvalue()
+        self.assertEqual(output, 1)
