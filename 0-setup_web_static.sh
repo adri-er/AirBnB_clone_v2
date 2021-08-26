72809c88271a3da7ac8e1348197b0812c286ac6a
adri-er <adriana.er98@hotmail.com>
2021-08-26 04:01:27 +0000

    [FEATURE] (Script to create web server setup)

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Create script to set up a web server
+sudo apt-get update
+sudo apt-get install nginx -y
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Fake content" > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/server_name _;/a location /hbnb_static {alias /data/web_static/current/;}/' /etc/nginx/sites-available/default
+service nginx restart
