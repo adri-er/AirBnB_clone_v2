cc035e78f982e25c9e42fb5f8e15dfd2935c68e8
Adriana Echeverri <adriana.er98@hotmail.com>
2021-08-27 21:23:15 +0000

    [FIX] (Correction of symbolic link)

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" This module generates a .tgz archive from """
+from fabric.api import *
+import datetime
+import os.path
+env.hosts = ['34.73.212.97', '34.138.250.102']
+
+
+def do_pack():
+    """ Create pack of file .pgz """
+    date_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    name_file = "versions/web_static_" + date_time + ".tgz"
+    localpth = "web_static/"
+    local("mkdir -p versions")
+    result = local("tar -zcvf {} {}".format(name_file, localpth))
+    if result is None:
+        return None
+    else:
+        return name_file
+
+
+def do_deploy(archive_path):
+    """ Distribute archive to server """
+    file_name = archive_path.split("/")[-1]
+    file_name_w_ext = file_name.split(".")[0]
+
+    if os.path.exists(archive_path) is False:
+        return False
+    put(archive_path, "/tmp/")
+    run("mkdir -p /data/web_static/releases/{}".format(file_name_w_ext))
+    run("tar -xzf /tmp/{} -C {}".format(file_name,
+        "/data/web_static/releases/".format(file_name_w_ext)))
+    run("rm /tmp/{}".format(file_name))
+    # run("mv /data/web_static/releases/{}/web_static/*
+    # /data/web_static/releases/{}/".format(file_name_w_ext, file_name_w_ext))
+    cmd = "rm -rf /data/web_static/releases/"
+    run("{}{}/web_static".format(cmd, file_name_w_ext))
+    run("rm -rf /data/web_static/current")
+    command = "ln -s /data/web_static/releases/{}".format(file_name_w_ext)
+    command += " /data/web_static/current"
+    run(command)
+    return True
