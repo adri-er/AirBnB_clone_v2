ab599c2ddfcecf2bc433d79dafd27a82baebd552
Adriana Echeverri <adriana.er98@hotmail.com>
2021-08-27 14:49:13 -0500

    [FIX] (Add directory)

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,8 +7,9 @@ import datetime
 def do_pack():
     """ Create pack of file .pgz """
     date_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    name_file = "web_static_" + date_time + ".tgz"
+    name_file = "versions/web_static_" + date_time + ".tgz"
     localpth = "web_static/"
+    local("mkdir -p versions")
     result = local("tar -zcvf {} {}".format(name_file, localpth))
     if result is None:
         return None
