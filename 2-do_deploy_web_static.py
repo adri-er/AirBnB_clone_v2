35c00bca1f242412d0acef0f19015351ca612e5b
Adriana Echeverri <adriana.er98@hotmail.com>
2021-08-28 12:41:13 -0500

    [STYLE] (pep8 Styling)

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,20 +22,20 @@ def do_pack():
 def do_deploy(archive_path):
     """ Distribute archive to server """
     file_name = archive_path.split("/")[-1]
-    file_name_w_ext = file_name.split(".")[0]
+    file_wext = file_name.split(".")[0]
     path = "/data/web_static/releases/"
     if os.path.exists(archive_path) is False:
         return False
 
     put(archive_path, "/tmp/")
-    run("mkdir -p {}{}".format(path, file_name_w_ext))
-    run("tar -xzf /tmp/{} -C {}{}".format(file_name, path, file_name_w_ext))
+    run("mkdir -p {}{}".format(path, file_wext))
+    run("tar -xzf /tmp/{} -C {}{}".format(file_name, path, file_wext))
     run("rm /tmp/{}".format(file_name))
-    run("mv {}{}/web_static/* {}{}/".format(path, file_name_w_ext, path, file_name_w_ext))
+    run("mv {}{}/web_static/* {}{}/".format(path, file_wext, path, file_wext))
     cmd = "rm -rf /data/web_static/releases/"
-    run("{}{}/web_static".format(cmd, file_name_w_ext))
+    run("{}{}/web_static".format(cmd, file_wext))
     run("rm -rf /data/web_static/current")
-    command = "ln -s /data/web_static/releases/{}".format(file_name_w_ext)
+    command = "ln -s /data/web_static/releases/{}".format(file_wext)
     command += " /data/web_static/current"
     run(command)
     return True
