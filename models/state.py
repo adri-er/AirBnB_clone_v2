cca342e1ba981abf797383b28aea4734b5124495
adri-er <adriana.er98@hotmail.com>
2021-09-12 16:05:35 +0000

    [REFACTOR] (Reorganize imports)

--- models/state.py
+++ models/state.py
@@ -4,21 +4,21 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-from models import storage
 import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', back_populates='state')
 
-    cities = relationship('City', back_populates='state')
-
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+    else:
         @property
         def cities(self):
             """ Getter cities """
+            from models import storage
             list_city = []
             for each_city in list(storage.all(City).values()):
                 if self.id == each_city.state_id:
